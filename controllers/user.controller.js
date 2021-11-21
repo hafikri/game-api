cd7e0db7b58f5b210bf3c8b98fdb2415d0d1a754
unknown <hafikri01@gmail.com>
2021-11-21 20:42:57 +0700

    add jwt_token pada dotenv add function login di user controller install package jsonwebtoken create schema login validate

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 
 const { User, UserRole } = require('../models')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
+// fungsi register
 exports.register = async (req, res, next) => {
     try {
         const { fullName, email, password, roleName } = req.body
@@ -44,6 +46,54 @@ exports.register = async (req, res, next) => {
             code: 200,
             data: {
                 user
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+
+        const isExist = await User.findOne({
+            where: {
+                email
+            },
+            include: [
+                {
+                    model: UserRole,
+                    as: 'role'
+                }
+            ]
+        })
+
+        if(!isExist) {
+            throw{
+                message: 'user not found',
+                code: 404,
+                error: 'bad request'
+            }
+        }
+
+        const isMatch = await bcrypt.compare(password, isExist.password)
+
+        if(!isMatch) {
+            throw {
+                message: 'invalid password',
+                code: 404,
+                error: 'bad request'
+            }
+        }
+
+        const token = jwt.sign({ userId: isExist.id, roleName: isExist.role.name }, process.env.JWT_TOKEN, {expiresIn: '7 days'})
+
+        return res.status(200).json({
+            message: 'success login',
+            code: 200,
+            data: {
+                token
             }
         })
     } catch (error) {
--- routers/user.router.js
+++ routers/user.router.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
 
-const { register } = require('../controllers/user.controller')
+const { register, login } = require('../controllers/user.controller')
 const { validate } = require('../middlewares/validation.middleware')
+const { loginSchema } = require('../schemas/login.schema')
 const { registSchema } = require('../schemas/register.schema')
 
 router.post('/register', validate(registSchema), register)
+
+router.post('/login', validate(loginSchema), login)
 
 module.exports = router
